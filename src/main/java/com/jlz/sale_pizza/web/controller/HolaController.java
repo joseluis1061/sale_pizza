d3c3d6f6d0476e5f3db6666acbf7971105addfa1
José Luis Zúñiga <jose@tangibledesign.co>
2024-09-24 16:36:11 -0500

    Add configuration

--- src/main/java/com/jlz/sale_pizza/web/controller/HolaController.java
+++ src/main/java/com/jlz/sale_pizza/web/controller/HolaController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("hola")
+@RequestMapping("saludar")
 public class HolaController {
   @GetMapping("/hola")
   public String saludar(){
